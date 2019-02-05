38a1097856c20f54ab5f457e1e49d420f4e27afc
Sander van den Oord <salieri78@hotmail.com>
2019-02-05 10:39:06 +0100

    first commit working version

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple app using Flask: building a bear character
+
+Following the Team Treehouse course on Flask Basics
+
---
+++ app.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import datetime
+import json
+
+from flask import (Flask, render_template, redirect,
+                   url_for, request, make_response)
+
+from options import DEFAULTS
+
+
+app = Flask(__name__)
+
+
+def get_saved_data():
+    """Return data from cookie if found, else empty dict"""
+    try:
+        data = json.loads(request.cookies.get('character'))
+    except TypeError:
+        data = {}
+    return data
+
+
+@app.route('/')
+def index():
+    """Show homepage with data from cookies"""
+    return render_template('index.html', saves=get_saved_data())
+
+
+@app.route('/builder')
+def builder():
+    return render_template(
+        'builder.html',
+        saves=get_saved_data(),
+        options=DEFAULTS,
+    )
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    """
+    Save input of submitted form to cookie and return index.html
+    """
+    data = get_saved_data()
+    data.update(dict(request.form.items()))
+
+    print(data)
+
+    response = make_response(redirect(url_for('builder')))
+
+    cookie_expire_date = datetime.datetime.now() + datetime.timedelta(days=90)
+
+    response.set_cookie(
+        'character',
+        json.dumps(data),
+        expires=cookie_expire_date,
+    )
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000)
